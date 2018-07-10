360c023c91d91251a212a8d7dc00568f25f6093f
takumi <takumi.waida@nts.ricoh.co.jp>
2018-07-10 22:24:40 +0900

    アップデート

--- 02_センサ評価/d05_heimannセンサ/main_mpl.py
+++ 02_センサ評価/d05_heimannセンサ/main_mpl.py
@@ -11,7 +11,7 @@ import time
 import itertools
 from pkg_read import sensor_mdl
 from pkg_read import machine_mdl
-from pkg_plot_qt import plot_mdl
+from pkg_plot import plot_mdl
 from pkg_config import sensor_cfg_mdl 
 from pkg_config import machine_cfg_mdl
 from pkg_config import plotter_cfg_mdl
@@ -53,21 +53,34 @@ sensor_number = sensor.senNum
 #%%グラフの初期化
 
 #描画条件指定
-mac_plot_key    = plotter_cfg.MACHINE_KEY
-sen_plot_key    = plotter_cfg.SENSOR_KEY
-dist_plot_key   = plotter_cfg.DIST_KEY
+machine_key = plotter_cfg.MACHINE_KEY
+sensor_key  = plotter_cfg.SENSOR_KEY
+dist_key  = plotter_cfg.DIST_KEY
 
 #グラフの初期化
-plotter.sensor_plot.init_line(sensor,sen_plot_key)
-plotter.machine_plot.init_line(machine,mac_plot_key)
-#plotter.dist_plot.init_line(dist_plot_key,plotter_cfg.SENPOS)
+plotter.sensor.init_line(sensor_key)
+plotter.machine.init_line(machine_key)
+plotter.dist.init_line(dist_key,plotter_cfg.SENPOS)
 
-#%%グラフ表示
+#%%繰り返し処理
 
-#表示スタート
-plotter.start()
+sta = time.time()
+while True:
+    #データ取得
+    sensor_data   = sensor.get_value()
+    machine_data  = machine.get_value()
+
+    #プロットの更新
+    plotter.sensor.update(sensor_data)
+    plotter.machine.update(machine_data)
+    plotter.dist.update(sensor_data)
+    
+    print(time.time()-sta)
+    sta = time.time()
+    # ESCキーが押されたら終了
+    if getkey(ESC):   
+        break
 
 #%%終了処理
-if getkey(ESC):   
-    sensor.stop() 
-    machine.stop()
+sensor.stop()
+machine.stop()
--- 02_センサ評価/d05_heimannセンサ/pkg_plot_qt/plot_mdl.py
+++ 02_センサ評価/d05_heimannセンサ/pkg_plot_qt/plot_mdl.py
@@ -6,7 +6,7 @@ Created on Fri Sep  8 11:04:01 2017
 
 
 """
-from PyQt5 import QMainWindow, QGridLayout
+from PyQt5 import QtWidgets as Qtw
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from pkg_plot_qt import plot_base_mdl as base_p
@@ -19,30 +19,27 @@ class Plotter():
 
     def __init__(self, cfg):
         self.pos = cfg.SENPOS        
-        
 
-    def InitUI(self):
+        self.gui = PlotGUI()
         
-        #ウィンドウ
-        self.win = QMainWindow()
-        self.plot  = pg.PlotWidget()
+        self.plotitem = self.gui.plotitem
         
-        #レイアウト
-        self.win.setCentoralWidget(self.plot)
+        #self.InitPlot()
+
         
+    def InitPlot(self):
+
         #グラフ生成
-        self.plt1=self.win.addPlot(0,0) #プロット配置
-        self.plt2=self.win.addPlot(0,1) #プロット配置
-        self.plt3=self.win.addPlot(1,0) #プロット配置
-        self.plt4=self.win.addPlot(1,1) #プロット配置
+        self.plt1=self.plotitem.plot(0,0) #プロット配置
+        self.plt2=self.plotitem.plot(0,1) #プロット配置
+        self.plt3=self.plotitem.plot(1,0) #プロット配置
+        self.plt4=self.plotitem.plot(1,1) #プロット配置
         
         #グラフ初期化
         self.sensor_plot     = SensorPlots (self.plt1)
         self.machine_plot    = MachinePlots(self.plt2) 
         #self.dist_plt       = DistPlots   (self.plt3)     
     
-        #表示開始
-        
     def start(self):
         self.timer=QtCore.QTimer()
         self.timer.timeout.connect(self.sensor_plot.update)
@@ -50,6 +47,54 @@ class Plotter():
         #self.timer.timeout.connect(self.dist_plt.update())
         self.timer.start(10)    #10msごとにupdateを呼び出し
 
+#%% GUI
+class PlotGUI():
+
+    def __init__(self):
+        self.InitUI()
+
+    def InitUI(self):
+        
+        #親ウィンドウ
+        win = Qtw.QMainWindow()
+        
+        #ファイルメニュー
+        menubar = self.win.menuBar()
+        fileMenu = menubar.addMenu('File')
+        
+        #ボタン
+        okButton = Qtw.QPushButton("OK")
+        stopButton = Qtw.QPushButton("Cancel")
+        hbox = Qtw.QHBoxLayout()
+        hbox.addWidget(okButton)
+        hbox.addWidget(stopButton)        
+        
+        #レイアウト
+        vbox = Qtw.QVBoxLayout()
+        vbox.addLayout(hbox)
+        
+        #プロットウィジェット
+        plotwidget  = pg.PlotWidget()
+        plotitem    = self.plotwidget.getPlotItem()
+        vbox.addWidget(plotwidget)
+        
+
+
+        
+        #ウィジェット領域
+        self.wid = Qtw.QWidget()
+        #ウィンドウにセット
+        self.win.setCentralWidget(self.wid)
+        
+
+        
+        
+        self.wid.setLayout(self.layout)
+
+
+        #ウィンドウ表示       
+        self.win.show()
+        
 #%%センサ時系列    
 class SensorPlots(base_p.Time_Plot):
         
