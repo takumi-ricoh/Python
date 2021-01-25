db03ff52dc470b84dc2ef4ef2adc87a11a98652e
takumiR <32160810+takumi-ricoh@users.noreply.github.com>
2021-01-25 10:53:16 +0900

    学習した系。久しぶり。

---
+++ 12_VisualStudioとgitに慣れる/01_hello_world/visualStudio_test01/visualStudio_test01/visualStudio_test01.py
@@ -0,0 +1,16 @@
+
+import sys
+from math import cos, radians
+import numpy as np
+
+print("Hellow World")
+
+#%% コードセル
+for i in range(360):
+	x = cos(radians(i))
+	#print(x)
+
+#%% 
+y = np.array([[1,2,4],[4,5,6],[7,8,9]])
+
+a=1
