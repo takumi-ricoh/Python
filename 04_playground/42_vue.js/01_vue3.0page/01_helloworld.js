a3843bbfe2239a3c97b86f1688624b2da8b5c434
takumi601 <takumi601@gmail.com>
2021-06-22 19:06:02 +0900

    vueの導入編

---
+++ 04_playground/42_vue.js/01_vue3.0page/01_helloworld.js
@@ -0,0 +1,8 @@
+// https://rightcode.co.jp/blog/information-technology/vue-js-introduction-hello-world
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World'
+    }
+})
